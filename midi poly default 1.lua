345652903b5da5d3330317833a4af657aa34300d
mudlogger <89759199+mudlogger@users.noreply.github.com>
2026-05-04 13:23:11 +0700

    Add files via upload

---
+++ midi poly default 1.lua
@@ -0,0 +1,129 @@
+patch.init = function()
+	ST["midi_mod_out_active"] = 0.0
+	P["spectre"] = 0.2854852
+	P["spectre3"] = 0.992157
+	P["notes"] = 0.6782392
+	P["spectre2"] = 0.8926285
+	P["phase"] = 0.3199321
+	ST["octave"] = 0.0
+	ST["midicc_select"] = 1.0
+	ST["quant_active"] = 0.0
+	_params["lens"].seq.start = 1; _params["lens"].seq.limit = 11
+	P["lens"] = 8.0
+	ST["midi_note_out_active"] = 0.0
+	P["length"] = 0.6268119
+	P["voicing"] = 0.5
+	P["tone"] = 0.0004123963
+	P["morph"] = 3.0
+	ST["midi_clk_out_active"] = 0.0
+	P["chord"] = 0.5
+	P["speed"] = 0.0004582181
+	P["tempo"] = 0.4108384
+	P["loop"] = 0.0
+	ST["mto_clock"] = 0.0
+	P["sustain"] = 0.5448672
+	ST["drone_active"] = 0.0
+	ST["global_tune"] = 0.0
+	P["time"] = 0.2193337
+	_params["scale"].seq.start = 1; _params["scale"].seq.limit = 4
+	P["scale"] = 1.0
+	ST["cv_select"] = 1.0
+	P["smode"] = 2.0
+	_params["shape"].seq.start = 1; _params["shape"].seq.limit = 11
+	P["shape"] = 5.0
+	P["lowcut"] = 0.0
+	ST["mto_notes"] = 1.0
+	P["stretchL"] = 0.5
+	ST["midi_dest"] = 1.0
+	P["glide"] = 1.0
+	_params["drone"].seq.start = 1; _params["drone"].seq.limit = 5
+	P["drone"] = 1.0
+	P["spectre1"] = 0.7339812
+	P["ramp"] = 0.0005651357
+	ST["current_extension"] = nil
+	P["timbre"] = 0.4587833
+	P["energy"] = 0.03488567
+	P["tmode"] = 2.0
+	P["stretchR"] = 0.5
+	Drone_chan_lookup[1] = {1, 0, 0, 0, 0}
+	Drone_chan_lookup[2] = {0, 1, 0, 0, 0}
+	Drone_chan_lookup[3] = {0, 0, 1, 0, 0}
+	Drone_chan_lookup[4] = {0, 0, 0, 1, 0}
+	Drone_chan_lookup[5] = {0, 0, 0, 0, 1}
+	Drone_chan_lookup[6] = {1, 0, 0, 0, 1}
+	Drone_chan_lookup[7] = {1, 1, 0, 0, 0}
+	Drone_chan_lookup[8] = {0, 1, 1, 0, 0}
+	Drone_chan_lookup[9] = {0, 0, 1, 1, 0}
+	Drone_chan_lookup[10] = {0, 0, 0, 1, 1}
+	Drone_chan_lookup[11] = {1, 1, 0, 1, 1}
+	_params.spectre1.vx = {0.1965743,0.4,0.3,0.03246399,0.8949211,0.5,0.6385801,0.7339812,0.241575,0.2657811,0.3746744}
+	_params.spectre2.vx = {0.3965743,0.5,0.4,0.03816823,1.0,0.5,0.6477395,0.8926285,0.5673178,0.4293588,0.4319967}
+	_params.spectre3.vx = {0.7965744,0.6,0.7,0.3675559,1.0,0.5,0.6477395,0.992157,0.8831592,0.604051,0.5013083}
+	Notes.Notes[1] = {0,2,4,5,7,9,11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[2] = {-3,-1,0,2,4,6,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[3] = {2,4,5,7,8,11,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[4] = {0,7,9,10,14,16,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[5] = {0,2,4,5,7,8,9,11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=8}
+	Notes.Notes[6] = {-12,2,0,4,0,7,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=8}
+	Notes.Notes[7] = {0.0,2.0391,3.863137,4.980451,7.019549,8.843586,10.88269,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[8] = {0.0,2.0391,2.94135,4.980451,7.019549,9.058649,9.9609,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[9] = {0,4,7,11,14,18,21,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,len=7}
+	Notes.Notes[10] = {-2,-2.2,-2,-1.8,-2,5,4.8,5,5.2,5,2,1.8,2,2.2,2,9,8.8,9,9.2,9,0,0,0,0,0,len=20}
+	Notes.Notes[11] = {0,-0.2,0,0.2,0,7,6.8,7,7.2,7,4,3.8,4,4.2,4,11,10.8,11,11.2,11,0,0,0,0,0,len=20}
+	MAP.link("midi_velocity","gate")
+	MAP.link("contour",-0.996976,"timbre")
+	MAP.link("midi_pitch","notes")
+	MAP.link("strum",2,"gate")
+	MAP.link("sustain","gate")
+	MAP.link("lfo",-0.16176,"tone")
+	MAP.link("lfo",-0.279967,"spectre2")
+	MAP.link("lfo",-0.713051,"energy")
+	MAP.link("lfo",-0.279967,"spectre1")
+	MAP.link("lfo",-0.279967,"spectre3")
+	MAP.link("notes",-0.177474,"sustain")
+	MAP.link("notes",-0.305093,"spectre2")
+	MAP.link("notes",-0.0205065,"tone")
+	MAP.link("notes",-0.305093,"spectre1")
+	MAP.link("notes",-0.305093,"spectre3")
+	MAP.link("clock",1,"speed")
+	gesture.G[1].mute = 0
+	gesture.G[1].flip = 0
+	gesture.G[1].time_source = "clock"
+	gesture.G[1].Mstep = false
+	gesture.G[1].Mlen = false
+	gesture.G[1].Mbeat = false
+	gesture.G[1].Mscan = false
+	gesture.G[1].Mlink = 0
+	gesture.G[2].mute = 0
+	gesture.G[2].flip = 0
+	gesture.G[2].time_source = "clock"
+	gesture.G[2].Mstep = false
+	gesture.G[2].Mlen = false
+	gesture.G[2].Mbeat = false
+	gesture.G[2].Mscan = false
+	gesture.G[2].Mlink = 0
+	gesture.G[3].mute = 0
+	gesture.G[3].flip = 0
+	gesture.G[3].time_source = "clock"
+	gesture.G[3].Mstep = false
+	gesture.G[3].Mlen = false
+	gesture.G[3].Mbeat = false
+	gesture.G[3].Mscan = false
+	gesture.G[3].Mlink = 0
+	gesture.G[4].mute = 0
+	gesture.G[4].flip = 0
+	gesture.G[4].time_source = "clock"
+	gesture.G[4].Mstep = false
+	gesture.G[4].Mlen = false
+	gesture.G[4].Mbeat = false
+	gesture.G[4].Mscan = false
+	gesture.G[4].Mlink = 0
+	gesture.G[5].mute = 0
+	gesture.G[5].flip = 0
+	gesture.G[5].time_source = "clock"
+	gesture.G[5].Mstep = false
+	gesture.G[5].Mlen = false
+	gesture.G[5].Mbeat = false
+	gesture.G[5].Mscan = false
+	gesture.G[5].Mlink = 0
+end
